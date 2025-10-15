e90f2d32c00871ac8c5ba717a4789016f8e174dd
WS-KSU-SWE <ws.ksu.swe@gmail.com>
2025-10-15 13:15:03 +0300

    Keyed Transposition done. Cryptanalysis almost

--- swe314_project_phase1/src/swe314_project_phase1/Main.java
+++ swe314_project_phase1/src/swe314_project_phase1/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!!!");
+		KeyedTranspositionEncryptDecrypt kt = new KeyedTranspositionEncryptDecrypt(4);
+		CryptanalysisDecryptor cd = new CryptanalysisDecryptor();
+		//kt.promptKey();
+		//kt.encryptWithSteps("Hello there friend!!!");
+		
 		
 	}
 	
